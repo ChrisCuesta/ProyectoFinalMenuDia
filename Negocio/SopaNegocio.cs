66c65252f4b73711ae6c4a2d036421b69b2c51da
Chris Cuesta <chriscuesta02@hotmail.com>
2022-01-26 19:18:30 -0500

    documentando metodos de clase negocio

--- Negocio/SopaNegocio.cs
+++ Negocio/SopaNegocio.cs
@@ -8,8 +8,16 @@ using DatosLinq;
 
 namespace Negocio
 {
+    /// <summary>
+    /// clase para el negocio de sopa
+    /// </summary>
     public static class SopaNegocio
     {
+        /// <summary>
+        /// Guardars the specified sopa dia.
+        /// </summary>
+        /// <param name="sopaDia">The sopa dia.</param>
+        /// <returns>devolverá una entidad sopa</returns>
         public static SopasEntidad Guardar(SopasEntidad sopaDia)
         {
             if (sopaDia.IdSopa == 0)
@@ -21,14 +29,28 @@ namespace Negocio
                 return SopasDatos.Actualizar(sopaDia);
             }
         }
+        /// <summary>
+        /// Devolvers the lista sopas.
+        /// </summary>
+        /// <returns>Devolvers the lista sopas.</returns>
         public static List<SopasEntidad> DevolverListaSopas()
         {
             return SopasDatos.DevolverSopas();
         }
+        /// <summary>
+        /// Devolvers the sopas por identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Devolvers the sopas por identifier</returns>
         public static SopasEntidad DevolverSopasPorId(int id)
         {
             return SopasDatos.DevolverSopasPorId(id);
         }
+        /// <summary>
+        /// Devolvers the ingredientes sopas por identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Devolvers the ingredientes sopas por identifier.</returns>
         public static SopasEntidad DevolverIngredientesSopasPorId(int id)
         {
             return SopasDatos.DevolverIngredientesSopasPorId(id);
