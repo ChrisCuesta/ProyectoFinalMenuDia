dfe099120a039724f37983de9b7f69d994b02be8
Chris Cuesta <chriscuesta02@hotmail.com>
2022-01-26 19:18:30 -0500

    Documentando clase segundoNegocio

--- Negocio/SegundoNegocio.cs
+++ Negocio/SegundoNegocio.cs
@@ -7,8 +7,16 @@ using Entidades;
 using DatosLinq;
 namespace Negocio
 {
+    /// <summary>
+    /// clase para el negocio de segundo
+    /// </summary>
     public static class SegundoNegocio
     {
+        /// <summary>
+        /// Guardars the specified segundo.
+        /// </summary>
+        /// <param name="segundo">The segundo.</param>
+        /// <returns>return entidad segundo</returns>
         public static SegundosEntidad Guardar(SegundosEntidad segundo)
         {
             if (segundo.IdSegundo == 0)
@@ -20,18 +28,37 @@ namespace Negocio
                 return SegundosDatos.Actualizar(segundo);
             }
         }
+        /// <summary>
+        /// Devolvers the lista segundos.
+        /// </summary>
+        /// <returns>return the lista segundos.</returns>
         public static List<SegundosEntidad> DevolverListaSegundos()
         {
             return SegundosDatos.DevolverSegundos();
         }
+        /// <summary>
+        /// Devolvers the segundos por identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>return the segundos por identifier.</returns>
         public static SegundosEntidad DevolverSegundosPorId(int id)
         {
             return SegundosDatos.DevolverSegundoPorId(id);
         }
+        /// <summary>
+        /// Devolvers the ingredientes segundos por identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns> return the ingredientes segundos por identifier.</returns>
         public static SegundosEntidad DevolverIngredientesSegundosPorId(int id)
         {
             return SegundosDatos.DevolverIngredientesSegundosPorId(id);
         }
+        /// <summary>
+        /// Eliminars the segundo.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Eliminars the segundo.</returns>
         public static bool EliminarSegundo(int id)
         {
             return SegundosDatos.EliminarSegundo(id);
