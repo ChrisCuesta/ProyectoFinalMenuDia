83160edd9f094ed790934a09635e0f6b670cf741
Chris Cuesta <chriscuesta02@hotmail.com>
2022-01-30 23:26:48 -0500

    Documentar clase MenuDiaJoin

--- Entidades/MenuDiaJoin.cs
+++ Entidades/MenuDiaJoin.cs
@@ -8,11 +8,21 @@ namespace Entidades
 {
     public class MenuDiaJoin
     {
-        
+        /// <summary>
+        /// Se crea el constructor vacio de la entidad MenuDiaJoin
+        /// </summary>
         public MenuDiaJoin()
         {
         }
-
+        /// <summary>
+        /// Se crea el constructor con parámetros de la entidad MenuDiaJoin
+        /// </summary>
+        /// <param name="numero">El número de menú</param>
+        /// <param name="sopa">La sopa del menú</param>
+        /// <param name="segundo">El segundo del menú</param>
+        /// <param name="bebida">La bebida del menú</param>
+        /// <param name="entrada">La entrada del menú</param>
+        /// <param name="fecha">La fecha que se ofrece el menú</param>
         public MenuDiaJoin(int numero, string sopa, string segundo, string bebida, string entrada, DateTime fecha)
         {
             Numero = numero;
@@ -22,7 +32,9 @@ namespace Entidades
             Entrada = entrada;
             Fecha = fecha;
         }
-
+        /// <summary>
+        /// Se encapsulan los atributos de la clase
+        /// </summary>
         public int Numero { get; set; }
         public string Sopa { get; set; }
         public string Segundo { get; set; }
